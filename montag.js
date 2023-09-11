a323b53baeb2df447ebfe1cb4e477e046e7d3b43
sunilthagyal93 <144680539+sunilthagyal93@users.noreply.github.com>
2023-09-11 16:19:40 +0530

    Create montag.js

---
+++ montag.js
@@ -0,0 +1,9 @@
+<!-- native bannr montag--->
+<script async="async" data-cfasync="false" src="//ophoacit.com/1?z=4764005"></script>
+
+<!-- banner montag---->
+    <script>(function(d,z,s){s.src='https://'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('asnincadar.com',3442672,document.createElement('script'))</script>
+
+
+<!-- in page push -->
+    <script>(function(d,z,s){s.src='https://'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('osspalkiaom.com',3442672,document.createElement('script'))</script>
